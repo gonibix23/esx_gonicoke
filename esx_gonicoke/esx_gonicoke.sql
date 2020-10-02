3823dc45cde5a265aaf769de722e7632751d2cc9
Gonzalo de Antonio <47611628+gonibix23@users.noreply.github.com>
2020-10-02 21:08:09 +0200

    Delete esx_gonicoke.sql

--- esx_gonicoke/esx_gonicoke.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `items` (name, label, weight, rare, can_remove) VALUES
-	('coca', 'Fardo de Coca', 1, 0, 1),
-	('usb', 'Usb con Información', 1, 0, 1),
-	('bolsacoca', 'Bolsa de Coca (10g)', 1, 0, 1),
-	('bolsa', 'Bolsa de Plastico', 1, 0, 1)
-;
